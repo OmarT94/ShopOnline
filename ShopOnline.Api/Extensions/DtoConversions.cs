59be87df7c2041a12e2046ef04de6a65fc0e035f
Omar Tamr <omrtmr94@gmail.com>
2022-02-24 17:09:34 +0100

    Projektdateien hinzufügen.

---
+++ ShopOnline.Api/Extensions/DtoConversions.cs
@@ -0,0 +1,45 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Extensions
+{
+    public static class DtoConversions
+    {
+        public static IEnumerable<ProductDto> ConvertToDo(this IEnumerable<Product> products,
+                                                          IEnumerable<ProductCategory> productCategories )
+        {
+            return (from product in products
+                    join productCategory in productCategories
+                    on product.CategoryId equals productCategory.Id
+                    select new ProductDto
+                    {
+                        Id = product.Id,
+                        Name = product.Name,
+                        Description = product.Description,
+                        ImageURL = product.ImageURL,
+                        Price = product.Price,
+                        Qty = product.Qty,
+                        CategoryId = product.CategoryId,
+                        CategoryName = productCategory.Name
+
+                    }).ToList();
+        }
+
+        public static ProductDto ConvertToDo(this Product product,
+                                                         ProductCategory productCategory)
+        {
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                ImageURL = product.ImageURL,
+                Price = product.Price,
+                Qty = product.Qty,
+                CategoryId = product.CategoryId,
+                CategoryName = productCategory.Name
+
+            };
+        }
+    }
+}
